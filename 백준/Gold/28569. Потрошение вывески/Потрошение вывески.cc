20ec975a5f5eaf52de9fd266b5ee1b95c2c903c5
kgw0513 <144788804+kgw0513@users.noreply.github.com>
2024-10-03 18:57:46 +0900

    [Gold I] Title: Потрошение вывески, Time: 120 ms, Memory: 2732 KB -BaekjoonHub

---
+++ 백준/Gold/28569. Потрошение вывески/Потрошение вывески.cc
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <cmath>
+#include <set>
+#include <map>
+#include <bitset>
+#include <utility>
+#include <assert.h>
+#include <unordered_set>
+#include <unordered_map>
+#pragma warning(disable:4996)
+#define fi first
+#define se second
+using namespace std;
+/*
+rope c++
+#include <ext/rope>
+using namespace __gnu_cxx;
+*/
+
+/*
+pbds c++ set전용
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+multiset을 쓰고 싶으면 위의 코드에서 마지막 줄만 아래 코드로 변경.(대신 erase가 정상작동 되지 않는다.)
+#define ordered_set tree<int, null_type, less_equal<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+이외에 범위를 늘리고 싶으면 타입변경하면 된다.
+ex. int->ll, less_equal<ll>
+*/
+
+const long long INF = 2147483647;
+const long long lINF = 9000000000000000000;
+const long long nlINF = lINF / 100;
+const int nINF = 1007483647;
+unsigned long long MOD1 = 1000000363;
+unsigned long long MOD2 = 1000000007;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<short, short>sh_sh;
+typedef pair<int, int> intint;
+typedef pair<int, double> int_d;
+typedef pair<double, double> dd;
+typedef pair<intint, double> int2_d;
+typedef pair<int, ll> intlong;
+typedef pair<ll, ll> ll_ll;
+typedef pair<intint, int> int2_int;
+typedef pair<int, intint> int_int2;
+typedef pair<ll, ll_ll> ll_ll2;
+typedef pair<ll_ll, ll>ll2_ll;
+typedef pair<intint, intint> int2_int2;
+typedef pair<ll_ll, ll_ll> ll2_ll2;
+typedef pair<char, int> char_int;
+
+ll arr1[201][201];
+bool used1[201][201];
+
+ll arr2[201][201];
+bool used2[201][201];
+
+ll dfs2(int, int);
+
+ll dfs1(int n, int m) {
+	if (used1[n][m])return arr1[n][m];
+	used1[n][m] = true;
+	if (n == 0)return arr1[n][m] = 0;
+	if (n == 1 || m == 0)return arr1[n][m] = 1;
+
+	ll ans = 0;
+	//행
+	{
+		for (int i = 1; i <= n; i++) {
+			ll a = dfs2(i - 1, m);
+
+			ll b = dfs1(n - i, m) + dfs2(n - i, m);
+
+			ans += (a * b);
+			ans %= MOD2;
+		}
+	}
+
+	return arr1[n][m] = ans;
+}
+ll dfs2(int n, int m) {
+	if (used2[n][m])return arr2[n][m];
+	used2[n][m] = true;
+
+	ll ans = 0;
+
+	if (m == 0)return arr2[n][m] = 0;
+	if (m == 1 || n == 0)return arr2[n][m] = 1;
+	
+	//열
+	{
+		for (int i = 1; i <= m; i++) {
+			ll a = dfs1(n, i-1);
+
+			ll b = dfs2(n, m - i) + dfs1(n, m - i);
+
+			ans += (a * b);
+			ans %= MOD2;
+		}
+	}
+
+	return arr2[n][m] = ans;
+}
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n, m;
+	cin >> n >> m;
+	cout << (dfs1(n, m) + dfs2(n, m)) % MOD2;
+}
