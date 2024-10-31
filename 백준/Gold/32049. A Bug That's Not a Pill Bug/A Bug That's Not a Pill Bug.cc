3013e1d6587306dc59008709ee76f38ddd08d590
kgw0513 <144788804+kgw0513@users.noreply.github.com>
2024-10-31 19:18:18 +0900

    [Gold IV] Title: A Bug That's Not a Pill Bug, Time: 8 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Gold/32049. A Bug That's Not a Pill Bug/A Bug That's Not a Pill Bug.cc
@@ -0,0 +1,128 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <cmath>
+#include <set>
+#include <map>
+#include <bitset>
+#include <utility>
+#include <assert.h>
+#include <unordered_set>
+#include <unordered_map>
+#pragma warning(disable:4996)
+#define fi first
+#define se second
+using namespace std;
+/*
+rope c++
+#include <ext/rope>
+using namespace __gnu_cxx;
+*/
+
+/*
+pbds c++ set전용
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+multiset을 쓰고 싶으면 위의 코드에서 마지막 줄만 아래 코드로 변경.(대신 erase가 정상작동 되지 않는다.)
+#define ordered_set tree<int, null_type, less_equal<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+이외에 범위를 늘리고 싶으면 타입변경하면 된다.
+ex. int->ll, less_equal<ll>
+*/
+
+const long long INF = 2147483647;
+const long long lINF = 9000000000000000000;
+const long long nlINF = lINF / 100;
+const int nINF = 1007483647;
+unsigned long long MOD1 = 1000000363;
+unsigned long long MOD2 = 1000000007;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<short, short>sh_sh;
+typedef pair<int, int> intint;
+typedef pair<int, double> int_d;
+typedef pair<double, double> dd;
+typedef pair<intint, double> int2_d;
+typedef pair<int, ll> intlong;
+typedef pair<ll, ll> ll_ll;
+typedef pair<intint, int> int2_int;
+typedef pair<int, intint> int_int2;
+typedef pair<ll, ll_ll> ll_ll2;
+typedef pair<ll_ll, ll>ll2_ll;
+typedef pair<intint, intint> int2_int2;
+typedef pair<ll_ll, ll_ll> ll2_ll2;
+typedef pair<char, int> char_int;
+
+intint dxdy[4] = { {1,0},{0,1},{-1,0},{0,-1} };
+int arr[4][101][101] = {};
+
+ll_ll cal(int n) {
+	ll_ll p;
+	ll dist;
+	int arrow = 0;
+	cin >> p.fi >> p.se >> dist;
+
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 101; j++) {
+			for (int k = 0; k < 101; k++) {
+				arr[i][j][k] = 0;
+			}
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		ll a, b;
+		cin >> a >> b;
+		for (int j = 0; j < 4; j++) {
+			arr[j][a][b] = -1; //장애물
+		}
+	}
+
+	ll move_count = 0;
+	while (dist > move_count) {
+		ll_ll new_p = { p.fi + dxdy[arrow].fi,p.se + dxdy[arrow].se };
+		if (new_p.fi > 100 || new_p.se > 100 ||
+			new_p.fi < 0 || new_p.se < 0) {
+			//밖으로 탈출했다는 뜻
+			ll left = dist - move_count;
+			return { p.fi + dxdy[arrow].fi * left,p.se + dxdy[arrow].se * left };
+		}
+		if (arr[arrow][new_p.fi][new_p.se] == -1) {
+			//앞이 장애물이면 왼쪽 회전
+			arrow++;
+			arrow %= 4;
+			continue;
+		}
+		move_count++;
+		if (arr[arrow][new_p.fi][new_p.se]!=0 && ((dist - arr[arrow][new_p.fi][new_p.se]) % (move_count - arr[arrow][new_p.fi][new_p.se]) == 0)) {
+			//무한반복해서 마지막으로 도달하는 곳
+			//cout << new_p.fi << "," << new_p.se << "찾음.\n";
+			//cout << dist << "-" << arr[arrow][new_p.fi][new_p.se] << "=>" << dist - arr[arrow][new_p.fi][new_p.se] << "인.\n";
+			//cout << arr[arrow][new_p.fi][new_p.se] << "값을 보유중으로 " << move_count << "-" << arr[arrow][new_p.fi][new_p.se] << "계산.\n";
+			return new_p;
+		}
+		arr[arrow][new_p.fi][new_p.se] = move_count; //방문기록
+		//cout << new_p.fi << "," << new_p.se << "/";
+		//cout << arr[arrow][new_p.fi][new_p.se] << "=" << move_count << "중,\n";
+		p = new_p;
+	}
+
+	return p;
+}
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n;
+	cin >> n;
+	while (n != 0) {
+		ll_ll h = cal(n);
+		cout << h.fi << " " << h.se << "\n";
+		cin >> n;
+	}
+}
