cd6b44c908914ce5d5d0c8552d9710c820260247
kgw0513 <144788804+kgw0513@users.noreply.github.com>
2024-11-11 19:17:31 +0900

    [Gold V] Title: 직육면체, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/27087. 직육면체/직육면체.cc
+++ 백준/Gold/27087. 직육면체/직육면체.cc
@@ -4,7 +4,6 @@ int main() {
     scanf("%d",&t);
 	while (t--) {
 		scanf("%d %d %d %d",&a,&b,&c,&p);
-		int cal = (a % p == 0) + (b % p == 0) + (c % p == 0);
-		printf("%c\n", "01"[cal >= 2]);
+		printf("%c\n","01"[!(a%p)+!(b%p)+!(c%p)>= 2]);
 	}
 }
