784804b5af2bbbd1002a4217c955bf20950c6dd2
kgw0513 <144788804+kgw0513@users.noreply.github.com>
2024-11-12 16:02:28 +0900

    [Gold IV] Title: 어려운 소인수분해, Time: 664 ms, Memory: 29640 KB -BaekjoonHub

---
+++ 백준/Gold/16563. 어려운 소인수분해/어려운 소인수분해.cc
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <cmath>
+#include <set>
+#include <map>
+#include <bitset>
+#include <utility>
+#include <assert.h>
+#include <unordered_set>
+#include <unordered_map>
+#pragma warning(disable:4996)
+#define fi first
+#define se second
+using namespace std;
+/*
+rope c++
+#include <ext/rope>
+using namespace __gnu_cxx;
+*/
+
+/*
+pbds c++ set전용
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+multiset을 쓰고 싶으면 위의 코드에서 마지막 줄만 아래 코드로 변경.(대신 erase가 정상작동 되지 않는다.)
+#define ordered_set tree<int, null_type, less_equal<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+이외에 범위를 늘리고 싶으면 타입변경하면 된다.
+ex. int->ll, less_equal<ll>
+*/
+
+const long long INF = 2147483647;
+const long long lINF = 9000000000000000000;
+const long long nlINF = lINF / 10000;
+const int nINF = 1007483647;
+unsigned long long MOD1 = 1000000363;
+unsigned long long MOD2 = 1000000007;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<short, short>sh_sh;
+typedef pair<int, int> intint;
+typedef pair<int, double> int_d;
+typedef pair<double, double> dd;
+typedef pair<intint, double> int2_d;
+typedef pair<int, ll> intlong;
+typedef pair<ll, ll> ll_ll;
+typedef pair<intint, int> int2_int;
+typedef pair<int, intint> int_int2;
+typedef pair<ll, ll_ll> ll_ll2;
+typedef pair<ll_ll, ll>ll2_ll;
+typedef pair<intint, intint> int2_int2;
+typedef pair<ll_ll, ll_ll> ll2_ll2;
+typedef pair<char, int> char_int;
+
+bool visit[5000001];
+vector<int>sosu;
+int last_p[5000001];
+int sum = 0;
+void dfs(int p, ll now_num,ll last_num) {
+	last_p[now_num] = last_num;
+	sum++;
+	for (int i = p; i < sosu.size(); i++) {
+		if (now_num * sosu[i] >= 5000001)return;
+		dfs(i, now_num * sosu[i], now_num);
+	}
+}
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int sqrt_n = sqrt(5000000);
+	for (int i = 2; i <= sqrt_n; i++) {
+		if (visit[i])continue;
+		for (int j = i * i; j < 5000001; j += i) {
+			visit[j] = true;
+		}
+	}
+	for (int i = 2; i <= 5000000; i++) {
+		if (visit[i])continue;
+		sosu.push_back(i);
+	}
+	for (int i = 0; i < sosu.size(); i++) {
+		dfs(i, sosu[i], -1);
+	}
+	int t, w;
+	cin >> t;
+	while (t--) {
+		cin >> w;
+		stack<int>ans;
+		while (last_p[w] != -1) {
+			assert(w >= 2);
+			ans.push(w / last_p[w]);
+			w = last_p[w];
+		}
+		ans.push(w);
+		while (ans.size()) {
+			cout << ans.top() << " ";
+			ans.pop();
+		}
+		cout << "\n";
+	}
+}
