12e228befadeec9ee2e1a546e55c83f19ec87d9e
kgw0513 <144788804+kgw0513@users.noreply.github.com>
2024-12-05 21:46:56 +0900

    [Gold V] Title: 나도리팡, Time: 16 ms, Memory: 2556 KB -BaekjoonHub

--- 백준/Gold/24508. 나도리팡/나도리팡.cc
+++ 백준/Gold/24508. 나도리팡/나도리팡.cc
@@ -63,16 +63,14 @@ typedef pair<char, int> char_int;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	ll n, k, t;
+	int n, k, t;
 	cin >> n >> k >> t;
-	deque<ll>arr(n);
-	for (ll& w : arr) {
+	deque<int>arr(n);
+	for (int& w : arr) {
 		cin >> w;
 	}
 	sort(arr.begin(), arr.end());
 	while (arr.size() && arr.front() == 0)arr.pop_front();
-	while (arr.size() && arr.back() == k)arr.pop_back();
-
 	while (arr.size() >= 2) {
 		int need_pop = min(arr[0],k - arr[arr.size() - 1]);
 		arr[0] -= need_pop;
@@ -80,6 +78,7 @@ int main() {
 		if (arr.back() == k)arr.pop_back();
 		if (arr.front() == 0)arr.pop_front();
 		t -= need_pop;
+        if(t<0)break;
 	}
 	if (arr.empty() && t>=0)cout << "YES";
 	else cout << "NO";
