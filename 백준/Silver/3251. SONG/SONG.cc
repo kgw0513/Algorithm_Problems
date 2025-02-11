1df1db31de55da6701d3f5bd7128e9c00de50cb5
kgw0513 <144788804+kgw0513@users.noreply.github.com>
2025-02-11 16:14:13 +0900

    [Silver IV] Title: SONG, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/3251. SONG/SONG.cc
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <cmath>
+#include <set>
+#include <map>
+#include <bitset>
+#include <utility>
+#include <assert.h>
+#include <unordered_set>
+#include <unordered_map>
+#pragma warning(disable:4996)
+#define fi first
+#define se second
+using namespace std;
+/*
+rope c++
+#include <ext/rope>
+using namespace __gnu_cxx;
+*/
+
+/*
+pbds c++ set전용
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+multiset을 쓰고 싶으면 위의 코드에서 마지막 줄만 아래 코드로 변경.(대신 erase가 정상작동 되지 않는다.)
+#define ordered_set tree<int, null_type, less_equal<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+이외에 범위를 늘리고 싶으면 타입변경하면 된다.
+ex. int->ll, less_equal<ll>
+*/
+
+constexpr double PI = 3.1415926535;
+constexpr long long INF = 2147483647;
+constexpr long long lINF = 9000000000000000000;
+constexpr long long nlINF = lINF / 10000;
+constexpr int nINF = 1007483647;
+constexpr unsigned long long MOD1 = 1000000363;
+constexpr unsigned long long MOD2 = 1000000007;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<short, short>sh_sh;
+typedef pair<int, int> intint;
+typedef pair<int, double> int_d;
+typedef pair<double, double> dd;
+typedef pair<intint, double> int2_d;
+typedef pair<int, ll> intlong;
+typedef pair<ll, ll> ll_ll;
+typedef pair<intint, int> int2_int;
+typedef pair<int, intint> int_int2;
+typedef pair<ll, ll_ll> ll_ll2;
+typedef pair<ll_ll, ll>ll2_ll;
+typedef pair<intint, intint> int2_int2;
+typedef pair<ll_ll, ll_ll> ll2_ll2;
+typedef pair<char, int> char_int;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t;
+	cin >> t;
+	cin.ignore();
+	while (t--) {
+		string w;
+		vector<string>arr;
+		for (int i = 0; i < 4; i++) {
+			getline(cin, w);
+			string h = "";
+			for (int j = 0; j < w.size(); j++) {
+				if ('A' <= w[j] && w[j] <= 'Z') {
+					w[j] = w[j] - 'A' + 'a';
+				}
+				if (w[j] == ' ') {
+					h = "";
+					continue;
+				}
+				if (w[j] == 'a' || w[j] == 'e' || w[j] == 'i' || w[j] == 'o' || w[j] == 'u') {
+					h = w[j];
+					continue;
+				}
+				h += w[j];
+			}
+			if (h.empty())return -1;
+			arr.push_back(h);
+		}
+
+		if (arr[0] == arr[1] && arr[1] == arr[2] && arr[2] == arr[3]) {
+			cout << "perfect\n";
+			continue;
+		}
+		if (arr[0] == arr[1] && arr[2] == arr[3]) {
+			cout << "even\n";
+			continue;
+		}
+		if (arr[0] == arr[2] && arr[1] == arr[3]) {
+			cout << "cross\n";
+			continue;
+		}
+		if (arr[0] == arr[3] && arr[1] == arr[2]) {
+			cout << "shell\n";
+			continue;
+		}
+		cout << "free\n";
+	}
+}
