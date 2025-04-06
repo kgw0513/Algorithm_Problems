7712df9699fb9fff448fad591e88869b17525184
kgw0513 <144788804+kgw0513@users.noreply.github.com>
2025-04-06 16:01:58 +0900

    [Silver I] Title: Alien Language (Large), Time: 412 ms, Memory: 16424 KB -BaekjoonHub

---
+++ 백준/Silver/12656. Alien Language （Large）/Alien Language （Large）.cc
@@ -0,0 +1,129 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <cmath>
+#include <set>
+#include <map>
+#include <bitset>
+#include <utility>
+#include <assert.h>
+#include <unordered_set>
+#include <unordered_map>
+#pragma warning(disable:4996)
+#define fi first
+#define se second
+using namespace std;
+/*
+rope c++
+#include <ext/rope>
+using namespace __gnu_cxx;
+*/
+
+/*
+pbds c++ set전용
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+multiset을 쓰고 싶으면 위의 코드에서 마지막 줄만 아래 코드로 변경.(대신 erase가 정상작동 되지 않는다.)
+#define ordered_set tree<int, null_type, less_equal<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+이외에 범위를 늘리고 싶으면 타입변경하면 된다.
+ex. int->ll, less_equal<ll>
+*/
+
+constexpr double PI = 3.1415926535;
+constexpr long long INF = 2147483647;
+constexpr long long lINF = 9000000000000000000;
+constexpr long long nlINF = lINF / 10000;
+constexpr int nINF = 1007483647;
+constexpr unsigned long long MOD1 = 1000000363;
+constexpr unsigned long long MOD2 = 1000000007;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<short, short>sh_sh;
+typedef pair<int, int> intint;
+typedef pair<int, double> int_d;
+typedef pair<double, double> dd;
+typedef pair<intint, double> int2_d;
+typedef pair<int, ll> intlong;
+typedef pair<ll, ll> ll_ll;
+typedef pair<intint, int> int2_int;
+typedef pair<int, intint> int_int2;
+typedef pair<ll, ll_ll> ll_ll2;
+typedef pair<ll_ll, ll>ll2_ll;
+typedef pair<intint, intint> int2_int2;
+typedef pair<ll_ll, ll_ll> ll2_ll2;
+typedef pair<char, int> char_int;
+
+struct Trie {
+	Trie* arr[26] = {};
+
+	void insert_string(const string& w,int p) {
+		if (p >= w.size()) {
+			return;
+		}
+		if (arr[w[p] - 'a'] == nullptr) {
+			arr[w[p] - 'a'] = new Trie();
+		}
+		arr[w[p] - 'a']->insert_string(w, p + 1);
+	}
+};
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	Trie* root_Trie = new Trie();
+
+	int l, d, n;
+	cin >> l >> d >> n;
+	string w;
+	while (d--) {
+		cin >> w;
+		root_Trie->insert_string(w, 0);
+	}
+
+	for(int test=1;test<=n;test++){
+		deque<Trie*>now_state;
+		now_state.push_back(root_Trie);
+		deque<Trie*>new_state;
+		cin >> w;
+		bool is_in = false;
+		for (int i = 0; i < w.size(); i++) {
+			if (w[i] == '(') {
+				new_state.clear();
+				is_in = true;
+				continue;
+			}
+			else if (w[i] == ')') {
+				now_state = new_state;
+				is_in = false;
+				continue;
+			}
+
+			if (is_in) {
+				for (int j = 0; j < now_state.size(); j++) {
+					if (now_state[j]->arr[w[i] - 'a'] != nullptr) {
+						new_state.push_back(now_state[j]->arr[w[i] - 'a']);
+					}
+				}
+			}
+			else {
+				new_state.clear();
+				for (int j = 0; j < now_state.size(); j++) {
+					if (now_state[j]->arr[w[i] - 'a'] != nullptr) {
+						new_state.push_back(now_state[j]->arr[w[i] - 'a']);
+					}
+				}
+				now_state = new_state;
+			}
+		}
+		cout << "Case #" << test << ": ";
+		cout << now_state.size() << "\n";
+	}
+}
