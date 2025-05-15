b629839aeb283103d33bf235566b17d6a81e89ce
kgw0513 <144788804+kgw0513@users.noreply.github.com>
2025-05-15 18:13:10 +0900

    [Platinum V] Title: 히스토그램에서 가장 큰 직사각형, Time: 36 ms, Memory: 2540 KB -BaekjoonHub

---
+++ 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.cc
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <cmath>
+#include <set>
+#include <map>
+#include <bitset>
+#include <utility>
+#include <assert.h>
+#include <unordered_set>
+#include <unordered_map>
+#pragma warning(disable:4996)
+#define fi first
+#define se second
+using namespace std;
+/*
+rope c++
+#include <ext/rope>
+using namespace __gnu_cxx;
+*/
+
+/*
+pbds c++ set전용
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+multiset을 쓰고 싶으면 위의 코드에서 마지막 줄만 아래 코드로 변경.(대신 erase가 정상작동 되지 않는다.)
+#define ordered_set tree<int, null_type, less_equal<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+이외에 범위를 늘리고 싶으면 타입변경하면 된다.
+ex. int->ll, less_equal<ll>
+*/
+
+constexpr double PI = 3.1415926535;
+constexpr long long INF = 2147483647;
+constexpr long long lINF = 9000000000000000000;
+constexpr long long nlINF = lINF / 10000;
+constexpr int nINF = 1007483647;
+constexpr unsigned long long MOD1 = 1000000007;
+constexpr unsigned long long MOD2 = 1000000009;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<short, short>sh_sh;
+typedef pair<int, int> intint;
+typedef pair<int, double> int_d;
+typedef pair<double, double> dd;
+typedef pair<intint, double> int2_d;
+typedef pair<int, ll> intlong;
+typedef pair<ll, ll> ll_ll;
+typedef pair<intint, int> int2_int;
+typedef pair<int, intint> int_int2;
+typedef pair<ll, ll_ll> ll_ll2;
+typedef pair<ll_ll, ll>ll2_ll;
+typedef pair<intint, intint> int2_int2;
+typedef pair<ll_ll, ll_ll> ll2_ll2;
+typedef pair<char, int> char_int;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	ll n, w;
+	while (1) {
+		deque<ll_ll>arr;//{들어온 위치,높이}
+		cin >> n;
+		if (n == 0)break;
+		ll ans = 0;
+		for (int i = 1; i <= n; i++) {
+			cin >> w;
+			ll min_p = i;
+			while (arr.size() && arr.back().se >= w) {
+				min_p = arr.back().fi;
+				ans = max(ans, (i - min_p) * arr.back().se);
+				arr.pop_back();
+			}
+			ans = max(ans, (i - min_p + 1) * w);
+			arr.push_back({ min_p,w });
+		}
+
+		for (int i = 0; i < arr.size(); i++) {
+			ans = max(ans, (n - arr[i].fi + 1) * arr[i].se);
+		}
+		cout << ans << "\n";
+	}
+}
