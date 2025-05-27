10afb83c0176339b6f4cb56da0f6b30a5aebec4a
kgw0513 <144788804+kgw0513@users.noreply.github.com>
2025-05-27 20:36:15 +0900

    [Gold IV] Title: RGB거리 2, Time: 0 ms, Memory: 2044 KB -BaekjoonHub

---
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.cc
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <cmath>
+#include <set>
+#include <map>
+#include <bitset>
+#include <utility>
+#include <assert.h>
+#include <unordered_set>
+#include <unordered_map>
+#pragma warning(disable:4996)
+#define fi first
+#define se second
+using namespace std;
+/*
+rope c++
+#include <ext/rope>
+using namespace __gnu_cxx;
+*/
+
+/*
+pbds c++ set전용
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+multiset을 쓰고 싶으면 위의 코드에서 마지막 줄만 아래 코드로 변경.(대신 erase가 정상작동 되지 않는다.)
+#define ordered_set tree<int, null_type, less_equal<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+이외에 범위를 늘리고 싶으면 타입변경하면 된다.
+ex. int->ll, less_equal<ll>
+*/
+
+constexpr double PI = 3.1415926535;
+constexpr long long INF = 2147483647;
+constexpr long long lINF = 9000000000000000000;
+constexpr long long nlINF = lINF / 10000;
+constexpr int nINF = 1007483647;
+constexpr unsigned long long MOD1 = 1000000007;
+constexpr unsigned long long MOD2 = 1000000009;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<short, short>sh_sh;
+typedef pair<int, int> intint;
+typedef pair<int, double> int_d;
+typedef pair<double, double> dd;
+typedef pair<intint, double> int2_d;
+typedef pair<int, ll> intlong;
+typedef pair<ll, ll> ll_ll;
+typedef pair<intint, int> int2_int;
+typedef pair<int, intint> int_int2;
+typedef pair<ll, ll_ll> ll_ll2;
+typedef pair<ll_ll, ll>ll2_ll;
+typedef pair<intint, intint> int2_int2;
+typedef pair<ll_ll, ll_ll> ll2_ll2;
+typedef pair<char, int> char_int;
+
+int n;
+int arr[3][1002];
+int state[3][1002];
+int cal(int start_p) {
+	for (int i = 0; i < 3; i++) {
+		arr[i][1] = nINF;
+	}
+	arr[start_p][1] = state[start_p][1];
+	for (int i = 2; i <= n; i++) {
+		for (int j = 0; j < 3; j++) {
+			arr[j][i] = nINF;
+			for (int k = 0; k < 3; k++) {
+				if (j == k)continue;
+				arr[j][i] = min(arr[j][i], arr[k][i-1]);
+				//cout << i << "," << j << "," << k << ":" << arr[k][i-1] << "/" << arr[j][i] << "\n";
+			}
+			arr[j][i] += state[j][i];
+		}
+	}
+	arr[start_p][n] = nINF;
+	//cout << start_p << ":" << arr[0][n]<<","<<arr[1][n] << "," << arr[2][n] << "\n";
+	return min({ arr[0][n],arr[1][n],arr[2][n] });
+}
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cin >> state[0][i] >> state[1][i] >> state[2][i];
+	}
+	int ans = min({ cal(0), cal(1), cal(2) });
+	cout << ans;
+}
