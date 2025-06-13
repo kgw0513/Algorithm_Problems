a3597757e711f6009c7c0d1252e9c94e17828716
kgw0513 <144788804+kgw0513@users.noreply.github.com>
2025-06-13 19:28:26 +0900

    [Gold V] Title: 등차수열 변환, Time: 12 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Gold/17088. 등차수열 변환/등차수열 변환.cc
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <cmath>
+#include <set>
+#include <map>
+#include <bitset>
+#include <utility>
+#include <assert.h>
+#include <unordered_set>
+#include <unordered_map>
+#pragma warning(disable:4996)
+#define fi first
+#define se second
+using namespace std;
+/*
+rope c++
+#include <ext/rope>
+using namespace __gnu_cxx;
+*/
+
+/*
+pbds c++ set전용
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+multiset을 쓰고 싶으면 위의 코드에서 마지막 줄만 아래 코드로 변경.(대신 erase가 정상작동 되지 않는다.)
+#define ordered_set tree<int, null_type, less_equal<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+이외에 범위를 늘리고 싶으면 타입변경하면 된다.
+ex. int->ll, less_equal<ll>
+*/
+
+constexpr double PI = 3.1415926535;
+constexpr long long INF = 2147483647;
+constexpr long long lINF = 9000000000000000000;
+constexpr long long nlINF = lINF / 10000;
+constexpr int nINF = 1007483647;
+constexpr unsigned long long MOD1 = 1000000007;
+constexpr unsigned long long MOD2 = 1000000009;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<short, short>sh_sh;
+typedef pair<int, int> intint;
+typedef pair<int, double> int_d;
+typedef pair<double, double> dd;
+typedef pair<intint, double> int2_d;
+typedef pair<int, ll> intlong;
+typedef pair<ll, ll> ll_ll;
+typedef pair<intint, int> int2_int;
+typedef pair<int, intint> int_int2;
+typedef pair<ll, ll_ll> ll_ll2;
+typedef pair<ll_ll, ll>ll2_ll;
+typedef pair<intint, intint> int2_int2;
+typedef pair<ll_ll, ll_ll> ll2_ll2;
+typedef pair<char, int> char_int;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n;
+	cin >> n;
+	if (n == 1) {
+		cout << 0;
+		return 0;
+	}
+
+	vector<ll>arr(n);
+	for (int i = 0; i < n; i++)cin >> arr[i];
+	
+	ll min_num = arr[1] - arr[0] - 2;
+	ll max_num = arr[1] - arr[0] + 2;
+	
+	ll ans = lINF;
+	//cout << min_num << "~" << max_num << "?\n";
+	for (ll x = min_num; x <= max_num; x++) {
+		for (ll start_num = arr[0] - 1; start_num <= arr[0] + 1; start_num++) {
+			ll now_num = start_num;
+			ll sum = 0;
+			if (start_num != arr[0])sum++;
+			bool is_can = true;
+			for (int i = 1; i < n; i++) {
+				ll gap = arr[i] - now_num;
+				if (x - gap < -1 || x - gap > 1) {
+					//cout << x << "," << start_num << "은 " << i << "/" << now_num << "에서 실패.(" << gap << ")\n";
+					is_can = false;
+					break;
+				}
+
+				if (x != gap)sum++;
+				now_num += x;
+			}
+			if (!is_can)continue;
+			ans = min(ans, sum);
+		}
+	}
+	cout << (ans == lINF ? -1 : ans);
+}
