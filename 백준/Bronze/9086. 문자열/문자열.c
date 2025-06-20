9c438c13e4595f5e221c1200d4d73bb0105be950
kgw0513 <144788804+kgw0513@users.noreply.github.com>
2025-06-21 02:35:05 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.c
+++ 백준/Bronze/9086. 문자열/문자열.c
@@ -1,11 +1,19 @@
 #pragma warning(disable:4996)
 #include <stdio.h>
 #include <string.h>
-char w[2];
+char w[18];
 int main() {
 	int n;
 	scanf("%d", &n);
-	while (scanf("%s", w)!=EOF) {
-		printf("%c%c\n", w[0],w[strlen(w)-1]);
+	getchar();
+	while (n--) {
+		fgets(w, sizeof(w), stdin);
+        int i=0;
+        printf("%c",w[0]);
+        for(i=0;('A'<=w[i] && w[i]<='Z');i++){
+            
+        }
+        printf("%c\n",w[i-1]);
+		memset(w, 0, sizeof(w));
 	}
 }
