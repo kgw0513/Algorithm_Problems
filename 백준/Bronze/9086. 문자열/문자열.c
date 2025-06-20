139fa9fc79bffe6e200fa835400454049bb2efff
kgw0513 <144788804+kgw0513@users.noreply.github.com>
2025-06-21 01:53:22 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.c
@@ -0,0 +1,11 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+#include <string.h>
+char w[1000];
+int main() {
+	int n;
+	scanf("%d", &n);
+	while (scanf("%s", w)!=EOF) {
+		printf("%c%c\n", w[0],w[strlen(w)-1]);
+	}
+}
