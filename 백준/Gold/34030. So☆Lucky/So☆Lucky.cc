affcc8a05db0f2e061eba8f22922a93a183303e6
kgw0513 <144788804+kgw0513@users.noreply.github.com>
2025-06-21 21:21:49 +0900

    [Gold III] Title: So☆Lucky, Time: 48 ms, Memory: 7380 KB -BaekjoonHub

---
+++ 백준/Gold/34030. So☆Lucky/So☆Lucky.cc
@@ -0,0 +1,149 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <cmath>
+#include <set>
+#include <map>
+#include <bitset>
+#include <utility>
+#include <assert.h>
+#include <unordered_set>
+#include <unordered_map>
+#pragma warning(disable:4996)
+#define fi first
+#define se second
+using namespace std;
+/*
+rope c++
+#include <ext/rope>
+using namespace __gnu_cxx;
+*/
+
+/*
+pbds c++ set전용
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+multiset을 쓰고 싶으면 위의 코드에서 마지막 줄만 아래 코드로 변경.(대신 erase가 정상작동 되지 않는다.)
+#define ordered_set tree<int, null_type, less_equal<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+이외에 범위를 늘리고 싶으면 타입변경하면 된다.
+ex. int->ll, less_equal<ll>
+*/
+
+constexpr double PI = 3.1415926535;
+constexpr long long INF = 2147483647;
+constexpr long long lINF = 9000000000000000000;
+constexpr long long nlINF = lINF / 10000;
+constexpr int nINF = 1007483647;
+constexpr unsigned long long MOD1 = 1000000007;
+constexpr unsigned long long MOD2 = 1000000009;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<short, short>sh_sh;
+typedef pair<int, int> intint;
+typedef pair<int, double> int_d;
+typedef pair<double, double> dd;
+typedef pair<intint, double> int2_d;
+typedef pair<int, ll> intlong;
+typedef pair<ll, ll> ll_ll;
+typedef pair<intint, int> int2_int;
+typedef pair<int, intint> int_int2;
+typedef pair<ll, ll_ll> ll_ll2;
+typedef pair<ll_ll, ll>ll2_ll;
+typedef pair<intint, intint> int2_int2;
+typedef pair<ll_ll, ll_ll> ll2_ll2;
+typedef pair<char, int> char_int;
+
+vector<int>ans;
+bool cal_odd(vector<int>&arr) {
+	deque<int>crr;
+	vector<int>drr;
+	for (int i = 0; i < arr.size(); i++) {
+		if (!crr.empty() &&(arr[i]%2 == crr.back() % 2)) {
+			crr.push_back(arr[i]);
+			continue;
+		}
+		int h = lower_bound(crr.begin(), crr.end(), arr[i])-crr.begin();
+		while (h > 0) {
+			h--;
+			drr.push_back(crr.front());
+			crr.pop_front();
+		}
+		if (!crr.empty()) {
+			drr.push_back(arr[i]);
+			continue;
+		}
+		crr.push_back(arr[i]);
+	}
+	while (!crr.empty()) {
+		drr.push_back(crr.front());
+		crr.pop_front();
+	}
+	bool is_match = true;
+	for (int i = 0; i < drr.size(); i++) {
+		if (ans[i] != drr[i]) {
+			is_match = false;
+			break;
+		}
+	}
+	return is_match;
+}
+
+bool cal_even(vector<int>& arr) {
+	deque<int>crr;
+	vector<int>drr;
+	for (int i = 0; i < arr.size(); i++) {
+		if (crr.empty() || (arr[i] % 2 == crr.back() % 2)) {
+			crr.push_back(arr[i]);
+			continue;
+		}
+		sort(crr.begin(), crr.end());
+		while (!crr.empty()) {
+			drr.push_back(crr.front());
+			crr.pop_front();
+		}
+        crr.push_back(arr[i]);
+	}
+	sort(crr.begin(), crr.end());
+	while (!crr.empty()) {
+		drr.push_back(crr.front());
+		crr.pop_front();
+	}
+	bool is_match = true;
+	for (int i = 0; i < drr.size(); i++) {
+		if (ans[i] != drr[i]) {
+			is_match = false;
+			break;
+		}
+	}
+	return is_match;
+}
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n;
+	cin >> n;
+	vector<int>arr(n);
+	vector<int>brr(n);
+	ans.resize(n);
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+		brr[i] = arr[i];
+		ans[i] = arr[i];
+	}
+	sort(ans.begin(), ans.end());
+
+	
+	if (cal_odd(arr))cout << "So Lucky\n";
+	else cout << "Unlucky\n";
+
+	if (cal_even(brr))cout << "So Lucky\n";
+	else cout << "Unlucky\n";
+}
