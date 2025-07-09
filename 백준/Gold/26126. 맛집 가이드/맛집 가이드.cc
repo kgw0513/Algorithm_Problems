2eceb11cf0ee9a08cfd0b390bd5cd549ca18420a
kgw0513 <144788804+kgw0513@users.noreply.github.com>
2025-07-09 23:31:42 +0900

    [Gold II] Title: 맛집 가이드, Time: 48 ms, Memory: 4464 KB -BaekjoonHub

---
+++ 백준/Gold/26126. 맛집 가이드/맛집 가이드.cc
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <cmath>
+#include <set>
+#include <map>
+#include <bitset>
+#include <utility>
+#include <assert.h>
+#include <unordered_set>
+#include <unordered_map>
+#pragma warning(disable:4996)
+#define fi first
+#define se second
+using namespace std;
+/*
+rope c++
+#include <ext/rope>
+using namespace __gnu_cxx;
+*/
+
+/*
+pbds c++ set전용
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+multiset을 쓰고 싶으면 위의 코드에서 마지막 줄만 아래 코드로 변경.(대신 erase가 정상작동 되지 않는다.)
+#define ordered_set tree<int, null_type, less_equal<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+이외에 범위를 늘리고 싶으면 타입변경하면 된다.
+ex. int->ll, less_equal<ll>
+*/
+
+constexpr double PI = 3.1415926535;
+constexpr long long INF = 2147483647;
+constexpr long long lINF = 9000000000000000000;
+constexpr long long nlINF = lINF / 10000;
+constexpr int nINF = 1007483647;
+constexpr long long MOD1 = 1000000007;
+constexpr long long MOD2 = 1000000009;
+constexpr long long MOD3 = 998244353;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<short, short>sh_sh;
+typedef pair<int, int> intint;
+typedef pair<int, double> int_d;
+typedef pair<double, double> dd;
+typedef pair<intint, double> int2_d;
+typedef pair<int, ll> intlong;
+typedef pair<ll, ll> ll_ll;
+typedef pair<intint, int> int2_int;
+typedef pair<int, intint> int_int2;
+typedef pair<ll, ll_ll> ll_ll2;
+typedef pair<ll_ll, ll>ll2_ll;
+typedef pair<intint, intint> int2_int2;
+typedef pair<ll_ll, ll_ll> ll2_ll2;
+typedef pair<char, int> char_int;
+
+int zero_state[200001];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, k, w;
+    cin >> n >> k;
+    vector<int>input;
+    for (int i = 0; i < n; i++) {
+        cin >> w;
+        input.push_back(w);
+    }
+
+    int ans = 0;
+    int not_zero = 0;
+    int counting = 0;
+    for (int i = 0; i < n; i++) {
+        if (zero_state[input[i]] == 0)not_zero++;
+        zero_state[input[i]]++;
+        if (zero_state[input[i]] == 0)not_zero--;
+
+        cin >> w;
+        if (zero_state[w] == 0)not_zero++;
+        zero_state[w]--;
+        if (zero_state[w] == 0)not_zero--;
+
+        counting++;
+        if (not_zero == 0 && counting>=k) {
+            ans++;
+            counting = 0;
+        }
+    }
+    cout << ans;
+}
