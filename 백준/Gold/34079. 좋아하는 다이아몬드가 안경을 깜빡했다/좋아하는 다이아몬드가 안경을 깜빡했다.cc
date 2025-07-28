9275f3acadfc04dc176084a1972edcfbb255795f
kgw0513 <144788804+kgw0513@users.noreply.github.com>
2025-07-28 17:32:23 +0900

    [Gold I] Title: 좋아하는 다이아몬드가 안경을 깜빡했다, Time: 112 ms, Memory: 14912 KB -BaekjoonHub

---
+++ 백준/Gold/34079. 좋아하는 다이아몬드가 안경을 깜빡했다/좋아하는 다이아몬드가 안경을 깜빡했다.cc
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <cmath>
+#include <set>
+#include <map>
+#include <bitset>
+#include <utility>
+#include <assert.h>
+#include <unordered_set>
+#include <unordered_map>
+#pragma warning(disable:4996)
+#define fi first
+#define se second
+using namespace std;
+/*
+rope c++
+#include <ext/rope>
+using namespace __gnu_cxx;
+*/
+ 
+/*
+pbds c++ set전용
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+ 
+multiset을 쓰고 싶으면 위의 코드에서 마지막 줄만 아래 코드로 변경.(대신 erase가 정상작동 되지 않는다.)
+#define ordered_set tree<int, null_type, less_equal<int>, rb_tree_tag,tree_order_statistics_node_update>
+ 
+이외에 범위를 늘리고 싶으면 타입변경하면 된다.
+ex. int->ll, less_equal<ll>
+*/
+ 
+constexpr double PI = 3.1415926535;
+constexpr long long INF = 2147483647;
+constexpr long long lINF = 9000000000000000000;
+constexpr long long nlINF = lINF / 10000;
+constexpr int nINF = 1007483647;
+constexpr long long MOD1 = 1000000007;
+constexpr long long MOD2 = 1000000009;
+constexpr long long MOD3 = 998244353;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<short, short>sh_sh;
+typedef pair<int, int> intint;
+typedef pair<int, double> int_d;
+typedef pair<double, double> dd;
+typedef pair<intint, double> int2_d;
+typedef pair<int, ll> intlong;
+typedef pair<ll, ll> ll_ll;
+typedef pair<intint, int> int2_int;
+typedef pair<int, intint> int_int2;
+typedef pair<ll, ll_ll> ll_ll2;
+typedef pair<ll_ll, ll>ll2_ll;
+typedef pair<intint, intint> int2_int2;
+typedef pair<ll_ll, ll_ll> ll2_ll2;
+typedef pair<char, int> char_int;
+ 
+int dp[200001];
+vector<int>lines[200001];
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, m, a, b;
+    cin >> n >> m;
+    while (m--) {
+        cin >> a >> b;
+        lines[a].push_back(b);
+        lines[b].push_back(a);
+    }
+    int visit_time = 0;
+    dp[1] = ++visit_time;
+    queue<int>arr;
+    arr.push(1);
+    while (dp[n] == 0) {
+        visit_time++;
+        int size = arr.size();
+        while (size--) {
+            int now_p = arr.front();
+            arr.pop();
+            for (int new_p : lines[now_p]) {
+                if (dp[new_p] != 0)continue;
+                dp[new_p] = visit_time;
+                arr.push(new_p);
+            }
+        }
+    }
+ 
+    while (arr.size())arr.pop();
+ 
+    arr.push(n);
+    while (dp[1] != 0) {
+        visit_time--;
+        int size = arr.size();
+        while (size--) {
+            int now_p = arr.front();
+            arr.pop();
+            for (int new_p : lines[now_p]) {
+                if (dp[new_p] != visit_time)continue;
+                dp[new_p] = 0;
+                arr.push(new_p);
+            }
+        }
+        if (arr.size() == 1) {
+            cout << arr.front();
+            return 0;
+        }
+    }
+    return -1;
+}
