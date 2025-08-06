29e3ddbb84e3698a82212a70694219b29b8b363e
kgw0513 <144788804+kgw0513@users.noreply.github.com>
2025-08-06 18:52:43 +0900

    [Bronze IV] Title: 치즈버거 만들기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/30017. 치즈버거 만들기/치즈버거 만들기.cc
+++ 백준/Bronze/30017. 치즈버거 만들기/치즈버거 만들기.cc
@@ -1,3 +1,3 @@
-#include <iostream>
-#include <algorithm>
-using namespace std;int main(){int a, b;cin >> a >> b;cout << min(a - 1, b) + min(b + 1, a);}
+#include<iostream>
+#include<algorithm>
+using namespace std;int main(){int a,b;cin>>a>>b;cout<<min(a-1,b)+min(b+1,a);}
