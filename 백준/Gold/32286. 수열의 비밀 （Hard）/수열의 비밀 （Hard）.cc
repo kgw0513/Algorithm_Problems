c724ae82d7cd5fa6f94bdfa66241fa79d309c0ce
kgw0513 <144788804+kgw0513@users.noreply.github.com>
2025-09-14 15:04:36 +0900

    [Gold III] Title: 수열의 비밀 (Hard), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/32286. 수열의 비밀 （Hard）/수열의 비밀 （Hard）.cc
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <cmath>
+#include <set>
+#include <map>
+#include <bitset>
+#include <utility>
+#include <assert.h>
+#include <unordered_set>
+#include <unordered_map>
+#pragma warning(disable:4996)
+#define fi first
+#define se second
+using namespace std;
+/*
+rope c++
+#include <ext/rope>
+using namespace __gnu_cxx;
+*/
+
+/*
+pbds c++ set전용
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+multiset을 쓰고 싶으면 위의 코드에서 마지막 줄만 아래 코드로 변경.(대신 erase가 정상작동 되지 않는다.)
+#define ordered_set tree<int, null_type, less_equal<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+이외에 범위를 늘리고 싶으면 타입변경하면 된다.
+ex. int->ll, less_equal<ll>
+*/
+
+constexpr double PI = 3.1415926535;
+constexpr long long INF = 2147483647;
+constexpr long long lINF = 9000000000000000000;
+constexpr long long nlINF = lINF / 10000;
+constexpr int nINF = 1007483647;
+constexpr long long MOD1 = 1000000007;
+constexpr long long MOD2 = 1000000009;
+constexpr long long MOD3 = 998244353;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<short, short>sh_sh;
+typedef pair<int, int> intint;
+typedef pair<int, double> int_d;
+typedef pair<double, double> dd;
+typedef pair<intint, double> int2_d;
+typedef pair<int, ll> intlong;
+typedef pair<ll, ll> ll_ll;
+typedef pair<intint, int> int2_int;
+typedef pair<int, intint> int_int2;
+typedef pair<ll, ll_ll> ll_ll2;
+typedef pair<ll_ll, ll>ll2_ll;
+typedef pair<intint, intint> int2_int2;
+typedef pair<ll_ll, ll_ll> ll2_ll2;
+typedef pair<char, int> char_int;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int k, p, q, r, s, a1;
+    cin >> k >> p >> q >> r >> s >> a1;
+    ll ans = 0;
+
+    ll pow_h = 1;
+    for (int i = 0; i <= k - 1; i++) {
+        ans += a1 * pow_h;
+        ans %= MOD1;
+        pow_h *= (p + r);
+        pow_h %= MOD1;
+    }
+
+    for (int i = 0; i <= k - 2; i++) {
+        pow_h = 1;
+        ll sum = 0;
+        for (int j = 0; j <= i; j++) {
+            sum += q * pow_h;
+            sum += s * pow_h;
+            sum %= MOD1;
+            pow_h *= (p + r);
+            pow_h %= MOD1;
+        }
+        ll two_pow_h = (1ll << (k - 2 - i));
+        two_pow_h %= MOD1;
+        sum *= two_pow_h;
+        sum %= MOD1;
+        ans += sum;
+        ans %= MOD1;
+    }
+
+    cout << ans;
+
+}
