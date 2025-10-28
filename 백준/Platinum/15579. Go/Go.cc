180070c2f34e57e64d181cf5f4252e58df49f70e
kgw0513 <144788804+kgw0513@users.noreply.github.com>
2025-10-28 14:22:56 +0900

    [Platinum IV] Title: Go, Time: 108 ms, Memory: 390604 KB -BaekjoonHub

---
+++ 백준/Platinum/15579. Go/Go.cc
@@ -0,0 +1,156 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <cmath>
+#include <set>
+#include <map>
+#include <bitset>
+#include <utility>
+#include <assert.h>
+#include <unordered_set>
+#include <unordered_map>
+#pragma warning(disable:4996)
+#define fi first
+#define se second
+using namespace std;
+/*
+rope c++
+#include <ext/rope>
+using namespace __gnu_cxx;
+*/
+
+/*
+pbds c++ set전용
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+multiset을 쓰고 싶으면 위의 코드에서 마지막 줄만 아래 코드로 변경.(대신 erase가 정상작동 되지 않는다.)
+#define ordered_set tree<int, null_type, less_equal<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+이외에 범위를 늘리고 싶으면 타입변경하면 된다.
+ex. int->ll, less_equal<ll>
+*/
+
+constexpr double PI = 3.1415926535;
+constexpr long long INF = 2147483647;
+constexpr long long lINF = 9000000000000000000;
+constexpr long long nlINF = lINF / 10000;
+constexpr int nINF = 1007483647;
+constexpr long long MOD1 = 1000000007;
+constexpr long long MOD2 = 1000000009;
+constexpr long long MOD3 = 998244353;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<short, short>sh_sh;
+typedef pair<int, int> intint;
+typedef pair<int, double> int_d;
+typedef pair<double, double> dd;
+typedef pair<intint, double> int2_d;
+typedef pair<int, ll> intlong;
+typedef pair<ll, ll> ll_ll;
+typedef pair<intint, int> int2_int;
+typedef pair<int, intint> int_int2;
+typedef pair<ll, ll_ll> ll_ll2;
+typedef pair<ll_ll, ll>ll2_ll;
+typedef pair<intint, intint> int2_int2;
+typedef pair<ll_ll, ll_ll> ll2_ll2;
+typedef pair<char, int> char_int;
+
+//* 포켓몬 번호가 0이라는건 없다는 뜻으로 처리하기.
+// 첫번째 index : 이제 갈 왼쪽 포켓몬
+// 두번째 index : 이제 갈 오른쪽 포켓몬
+// 세번째 index : 마지막 이동이 이제 갈 왼쪽 포켓몬 옆인지(0), 이제 갈 오른쪽 포켓몬 옆인지(1)
+// 네번째 index : 걸린시간 (최대 2000시간 쓴다 가정)
+// 저장 값 : 최대가 되는 사탕 갯수
+ll arr[105][105][2][2005];
+bool visit[105][105][2][2005];
+
+ll_ll pokemon[105];
+
+ll house2pokemon[1005];
+ll pokemon2house[105];
+
+ll pokemon2leftpokemon[105]; //현 포켓몬 -> 다음 왼쪽 포켓몬 번호
+ll pokemon2rightpokemon[105]; //현 포켓몬 -> 다음 오른쪽 포켓몬 번호
+
+ll dfs(ll poke_s, ll poke_e, ll now_p,bool is_left, ll cur_time) {
+    if (cur_time > 2000)return 0;
+
+    if (visit[poke_s][poke_e][is_left][cur_time])return arr[poke_s][poke_e][is_left][cur_time];
+    visit[poke_s][poke_e][is_left][cur_time] = true;
+
+    if (poke_s == 0 && poke_e == 0)return arr[poke_s][poke_e][is_left][cur_time] = 0;
+
+    if (poke_s != 0) {
+        ll next_p = pokemon2house[poke_s];
+        ll next_time = cur_time + abs(now_p - next_p);
+        arr[poke_s][poke_e][is_left][cur_time] = max(arr[poke_s][poke_e][is_left][cur_time],dfs(pokemon2leftpokemon[poke_s], poke_e, next_p, true, next_time) + (next_time < pokemon[poke_s].se ? pokemon[poke_s].fi : 0));
+    }
+
+    if (poke_e != 0) {
+        ll next_p = pokemon2house[poke_e];
+        ll next_time = cur_time + abs(next_p - now_p);
+        arr[poke_s][poke_e][is_left][cur_time] = max(arr[poke_s][poke_e][is_left][cur_time], dfs(poke_s, pokemon2rightpokemon[poke_e], next_p, false, next_time) + (next_time < pokemon[poke_e].se ? pokemon[poke_e].fi : 0));
+    }
+    
+    //cout << poke_s << "~" << poke_e << "/ 왼 : " << is_left << " / 시간 : " << cur_time << " = " << arr[poke_s][poke_e][is_left][cur_time] << "??\n";
+    return arr[poke_s][poke_e][is_left][cur_time];
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll ans = 0;
+    ll n, k, m, a, b, c;
+    cin >> n >> k >> m;
+    for(int num=1;num<=m;num++){
+        cin >> a >> b >> c;
+        if (a == k) { //시작위치에 포켓몬 있으면 그냥 잡으면 되니 처리 제외.
+            ans += b;
+            continue;
+        }
+        pokemon[num] = { b,c };
+        house2pokemon[a] = num;
+        pokemon2house[num] = a;
+    }
+
+    {
+        stack<ll>brr;
+        for (int i = 1; i <= n; i++) {
+            if (house2pokemon[i] == 0)continue;
+            if (!brr.empty()) {
+                int s = brr.top();
+                int e = house2pokemon[i];
+                
+                pokemon2leftpokemon[e] = s;
+                pokemon2rightpokemon[s] = e;
+            }
+            brr.push(house2pokemon[i]);
+        }
+    }
+
+    ll_ll start_set = { 0,0 };
+    for (int i = k; i >= 1; i--) {
+        if (house2pokemon[i] != 0) {
+            start_set.fi = house2pokemon[i];
+            break;
+        }
+    }
+    for (int i = k; i <= n; i++) {
+        if (house2pokemon[i] != 0) {
+            start_set.se = house2pokemon[i];
+            break;
+        }
+    }
+
+    //cout << start_set.fi << "~" << start_set.se << "!!\n";
+    ans += dfs(start_set.fi, start_set.se, k, true, 0);
+
+    cout << ans;
+}
---
+++ 백준/Platinum/15579. Go/README.md
@@ -0,0 +1,42 @@
+# [Platinum IV] Go - 15579 
+
+[문제 링크](https://www.acmicpc.net/problem/15579) 
+
+### 성능 요약
+
+메모리: 390604 KB, 시간: 108 ms
+
+### 분류
+
+다이나믹 프로그래밍
+
+### 제출 일자
+
+2025년 10월 28일 14:22:44
+
+### 문제 설명
+
+<p>Branimirko is still a passionate player of the world-renowned game Pokémon Go. Recently, he decided to organize a competition in catching Pokémon. It will be held in Ilica street in Zagreb, and the main sponsor is his friend Slavko. The reward is, of course, candy!</p>
+
+<p>Ilica is, as we all know, the longest street in Zagreb. There are N houses on the same side of the street, and each house has a house number between 1 and N. The competition begins at house number K.</p>
+
+<p>Before the competition, Branimirko looked at the map and saw M Pokémon. Each Pokémon is located at its (distinct) house number A<sub>i</sub>, is valued at B<sub>i</sub> candy, and can be caught only in the next T<sub>i</sub> seconds, after which it disappears from the map and is impossible to catch.</p>
+
+<p>Branimirko can visit one house number per second. Also, when he catches a Pokémon, that Pokémon disappears from the map.</p>
+
+<p>Since Branimirko really likes candy, he asked for your help.</p>
+
+<p>Help him and determine the maximal amount of candy he can get!</p>
+
+### 입력 
+
+ <p>The first line of input contains integers N, K (1 ≤ K ≤ N ≤ 1 000) and M (1 ≤ M ≤ 100), the number of houses, the starting house number and the number of Pokémon.</p>
+
+<p>Each of the following M lines contains integers A<sub>i</sub> (1 ≤ A<sub>i</sub> ≤ N), B<sub>i</sub> (1 ≤ B<sub>i</sub> ≤ 100) and T<sub>i</sub> (1 ≤ T<sub>i</sub> ≤ 2 000) from the task.</p>
+
+<p>In the input data, the Pokémon will always be in a strictly ascending order by house number A<sup>i</sup>.</p>
+
+### 출력 
+
+ <p>You must output the required maximal amount of candy from the task.</p>
+
