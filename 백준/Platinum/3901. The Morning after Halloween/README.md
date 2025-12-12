f8f081038fe7ecb82f66f661622738cef4f25d15
kgw0513 <144788804+kgw0513@users.noreply.github.com>
2025-12-13 03:03:07 +0900

    [Platinum V] Title: The Morning after Halloween, Time: 5684 ms, Memory: 76172 KB -BaekjoonHub

---
+++ 백준/Platinum/3901. The Morning after Halloween/README.md
@@ -0,0 +1,81 @@
+# [Platinum V] The Morning after Halloween - 3901 
+
+[문제 링크](https://www.acmicpc.net/problem/3901) 
+
+### 성능 요약
+
+메모리: 76172 KB, 시간: 5684 ms
+
+### 분류
+
+구현, 그래프 이론, 그래프 탐색, 너비 우선 탐색, 격자 그래프
+
+### 제출 일자
+
+2025년 12월 13일 03:02:53
+
+### 문제 설명
+
+<p>You are working for an amusement park as an operator of an obakeyashiki, or a haunted house, in which guests walk through narrow and dark corridors. The house is proud of their lively ghosts, which are actually robots remotely controlled by the operator, hiding here and there in the corridors. One morning, you found that the ghosts are not in the positions where they are supposed to be. Ah, yesterday was Halloween. Believe or not, paranormal spirits have moved them around the corridors in the night. You have to move them into their right positions before guests come. Your manager is eager to know how long it takes to restore the ghosts.</p>
+
+<p>In this problem, you are asked to write a program that, given a floor map of a house, finds the smallest number of steps to move all ghosts to the positions where they are supposed to be.</p>
+
+<p>A floor consists of a matrix of square cells. A cell is either a wall cell where ghosts cannot move into or a corridor cell where they can.</p>
+
+<p>At each step, you can move any number of ghosts simultaneously. Every ghost can either stay in the current cell, or move to one of the corridor cells in its 4-neighborhood (i.e. immediately left, right, up or down), if the ghosts satisfy the following conditions:</p>
+
+<ol>
+	<li>No more than one ghost occupies one position at the end of the step.</li>
+	<li>No pair of ghosts exchange their positions one another in the step.</li>
+</ol>
+
+<p>For example, suppose ghosts are located as shown in the following (partial) map, where a sharp sign (‘#’) represents a wall cell and ‘a’, ‘b’, and ‘c’ ghosts.</p>
+
+<pre>####
+ ab#
+#c##
+####
+</pre>
+
+<p>The following four maps show the only possible positions of the ghosts after one step.</p>
+
+<pre>####   ####   ####   ####
+ ab#   a b#   acb#   ab #
+#c##   #c##   # ##   #c##
+####   ####   ####   ####
+</pre>
+
+### 입력 
+
+ <p>The input consists of at most 10 datasets, each of which represents a floor map of a house. The format of a dataset is as follows.</p>
+
+<pre>w h n
+c11 c12 ··· c1w
+c21 c22 ··· c2w
+...
+ch1 ch2 ··· chw
+</pre>
+
+<p> </p>
+
+<p>w, h and n in the first line are integers, separated by a space. w and h are the floor width and height of the house, respectively. n is the number of ghosts. They satisfy the following constraints.</p>
+
+<p>4 ≤ w ≤ 16, 4 ≤ h ≤ 16, 1 ≤ n ≤ 3</p>
+
+<p>Subsequent h lines of w characters are the floor map. Each of cij is either:</p>
+
+<ul>
+	<li>a ‘#’ representing a wall cell,</li>
+	<li>a lowercase letter representing a corridor cell which is the initial position of a ghost,</li>
+	<li>an uppercase letter representing a corridor cell which is the position where the ghost corresponding to its lowercase letter is supposed to be, or</li>
+	<li>a space representing a corridor cell that is none of the above.</li>
+</ul>
+
+<p>In each map, each of the first n letters from a and the first n letters from A appears once and only once. Outermost cells of a map are walls; i.e. all characters of the first and last lines are sharps; and the first and last characters on each line are also sharps. All corridor cells in a map are connected; i.e. given a corridor cell, you can reach any other corridor cell by following corridor cells in the 4-neighborhoods. Similarly, all wall cells are connected. Any 2 × 2 area on any map has at least one sharp. You can assume that every map has a sequence of moves of ghosts that restores all ghosts to the positions where they are supposed to be.</p>
+
+<p>The last dataset is followed by a line containing three zeros separated by a space.</p>
+
+### 출력 
+
+ <p>For each dataset in the input, one line containing the smallest number of steps to restore ghosts into the positions where they are supposed to be should be output. An output line should not contain extra characters such as spaces.</p>
+
---
+++ 백준/Platinum/3901. The Morning after Halloween/The Morning after Halloween.cc
@@ -0,0 +1,174 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <cmath>
+#include <set>
+#include <map>
+#include <bitset>
+#include <utility>
+#include <assert.h>
+#include <unordered_set>
+#include <unordered_map>
+#include <complex>
+#pragma warning(disable:4996)
+#define fi first
+#define se second
+using namespace std;
+/*
+rope c++
+#include <ext/rope>
+using namespace __gnu_cxx;
+*/
+
+/*
+pbds c++ set전용
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+multiset을 쓰고 싶으면 위의 코드에서 마지막 줄만 아래 코드로 변경.(대신 erase가 정상작동 되지 않는다.)
+#define ordered_set tree<int, null_type, less_equal<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+이외에 범위를 늘리고 싶으면 타입변경하면 된다.
+ex. int->ll, less_equal<ll>
+*/
+
+constexpr double PI = 3.1415926535;
+constexpr long long INF = 2147483647;
+constexpr long long lINF = 9000000000000000000;
+constexpr long long nlINF = lINF / 10000;
+constexpr int nINF = 1007483647;
+constexpr long long MOD1 = 1000000007;
+constexpr long long MOD2 = 1000000009;
+constexpr long long MOD3 = 998244353;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<short, short>sh_sh;
+typedef pair<int, int> intint;
+typedef pair<int, double> int_d;
+typedef pair<double, double> dd;
+typedef pair<intint, double> int2_d;
+typedef pair<int, ll> intlong;
+typedef pair<ll, ll> ll_ll;
+typedef pair<intint, int> int2_int;
+typedef pair<int, intint> int_int2;
+typedef pair<ll, ll_ll> ll_ll2;
+typedef pair<ll_ll, ll>ll2_ll;
+typedef pair<intint, intint> int2_int2;
+typedef pair<ll_ll, ll_ll> ll2_ll2;
+typedef pair<char, int> char_int;
+typedef pair<int, char> int_char;
+typedef complex<double> cpx;
+
+//[a좌표][b좌표][c좌표]
+int dp[16][16][16][16][16][16];
+char state[16][16];
+
+intint dxdy[5] = { {-1,0},{1,0},{0,-1},{0,1},{0,0} };
+
+bool bIsBlock(intint p) {
+    if (state[p.fi][p.se] == '#')return true;
+    return false;
+}
+bool bIsCross(intint la, intint a, intint lb=intint({-1,-2}), intint b = intint({ -2,-3 }), intint lc = intint({ -3,-4 }), intint c = intint({ -4,-5 })) {
+    if (a == b || b == c || a == c)return true;
+    if (la == b && a == lb)return true;
+    if (la == c && a == lc)return true;
+    if (lb == c && b == lc)return true;
+    return false;
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, m, k;
+    while (1) {
+        for (int a = 0; a < 16; a++) {
+            for (int b = 0; b < 16; b++) {
+                for (int c = 0; c < 16; c++) {
+                    for (int d = 0; d < 16; d++) {
+                        for (int e = 0; e < 16; e++) {
+                            for (int f = 0; f < 16; f++) {
+                                dp[a][b][c][d][e][f] = nINF;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        cin >> m >> n >> k;
+        if (n == 0)break;
+        vector<intint>first_state(3, { 0,0 });
+        vector<intint>last_state(3, { 0,0 });
+        cin.ignore();
+        cin.ignore();
+        string w;
+        for (int i = 0; i < n; i++) {
+            //cout << i << "번째 줄\n";
+            getline(cin, w);
+            for (int j = 0; j < m; j++) {
+                state[i][j] = w[j];
+                //cout << i << "," << j << ":" << state[i][j] << "\n";
+                if (state[i][j] >= 'A' && state[i][j] <= 'C') {
+                    first_state[state[i][j] - 'A'] = { i,j };
+                    state[i][j] = '.';
+                }
+                else if (state[i][j] >= 'a' && state[i][j] <= 'c') {
+                    last_state[state[i][j] - 'a'] = { i,j };
+                    state[i][j] = '.';
+                }
+            }
+        }
+
+        queue<vector<intint>>arr;
+        arr.push(first_state);
+        dp[first_state[0].fi][first_state[0].se][first_state[1].fi][first_state[1].se][first_state[2].fi][first_state[2].se] = 0;
+
+        while (arr.size()) {
+            intint a = arr.front()[0];
+            intint b = arr.front()[1];
+            intint c = arr.front()[2];
+            arr.pop();
+            for (intint dxdy_a : dxdy) {
+                intint new_a = { a.fi + dxdy_a.fi,a.se + dxdy_a.se };
+                if (bIsBlock(new_a))continue;
+                if (k == 1) {
+                    if (bIsCross(a, new_a))continue;
+                    if (dp[new_a.fi][new_a.se][b.fi][b.se][c.fi][c.se] != nINF)continue;
+                    dp[new_a.fi][new_a.se][b.fi][b.se][c.fi][c.se] = dp[a.fi][a.se][b.fi][b.se][c.fi][c.se] + 1;
+                    arr.push({ new_a,b,c });
+                    continue;
+                }
+                for (intint dxdy_b : dxdy) {
+                    intint new_b = { b.fi + dxdy_b.fi,b.se + dxdy_b.se };
+                    if (bIsBlock(new_b))continue;
+                    if (k == 2) {
+                        if (bIsCross(a, new_a,b,new_b))continue;
+                        if (dp[new_a.fi][new_a.se][new_b.fi][new_b.se][c.fi][c.se] != nINF)continue;
+                        dp[new_a.fi][new_a.se][new_b.fi][new_b.se][c.fi][c.se] = dp[a.fi][a.se][b.fi][b.se][c.fi][c.se] + 1;
+                        arr.push({ new_a,new_b,c });
+                        continue;
+                    }
+                    for (intint dxdy_c : dxdy) {
+                        intint new_c = { c.fi + dxdy_c.fi,c.se + dxdy_c.se };
+                        if (bIsBlock(new_c))continue;
+                        if (k == 3) {
+                            if (bIsCross(a, new_a, b, new_b,c,new_c))continue;
+                            if (dp[new_a.fi][new_a.se][new_b.fi][new_b.se][new_c.fi][new_c.se] != nINF)continue;
+                            dp[new_a.fi][new_a.se][new_b.fi][new_b.se][new_c.fi][new_c.se] = dp[a.fi][a.se][b.fi][b.se][c.fi][c.se] + 1;
+                            arr.push({ new_a,new_b,new_c });
+                            continue;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (dp[last_state[0].fi][last_state[0].se][last_state[1].fi][last_state[1].se][last_state[2].fi][last_state[2].se] == nINF)return -1;
+        cout << dp[last_state[0].fi][last_state[0].se][last_state[1].fi][last_state[1].se][last_state[2].fi][last_state[2].se] << "\n";
+    }
+}
