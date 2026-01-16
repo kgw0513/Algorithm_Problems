76ea541bee783fa2921642f86b3bb0ac0135f316
kgw0513 <144788804+kgw0513@users.noreply.github.com>
2026-01-16 12:46:37 +0900

    [Platinum V] Title: Safe Passage, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Platinum/11092. Safe Passage/Safe Passage.cc
+++ 백준/Platinum/11092. Safe Passage/Safe Passage.cc
@@ -65,100 +65,36 @@ typedef pair<char, int> char_int;
 typedef pair<int, char> int_char;
 typedef complex<double> cpx;
 
-//[건너간 상태][망토가 기숙사에 있으면 1]
-ll dp[1 << 15][2];
-
-struct cmp {
-	bool operator()(ll2_ll& a, ll2_ll& b) {
-		return a.se > b.se;
-	}
-};
-
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	ll n;
+	int n;
 	cin >> n;
-	vector <ll> arr(n);
-	for (ll& w : arr) {
+	vector<int>arr(n);
+	for (int& w : arr) {
 		cin >> w;
 	}
 	sort(arr.begin(), arr.end());
-
-	for (int i = 0; i < (1 << 15); i++) {
-		for (int j = 0; j < 2; j++) {
-			dp[i][j] = nlINF;
-		}
+	if (n <= 2) {
+		cout << arr[n - 1];
+		return 0;
 	}
 
-	dp[0][0] = 0;
-
-	priority_queue<ll2_ll,vector<ll2_ll>,cmp>brr;
-	brr.push({ { 0,0 },0 });
-
-	while (!brr.empty()) {
-		ll_ll p = brr.top().fi;
-		ll cost = brr.top().se;
-		brr.pop();
-
-		if (dp[p.fi][p.se] != cost)continue;
-
-		//망토가 정문에 있을때
-		if (p.se == 0) {
-			for (int i = 0; i < n; i++) {
-				for (int j = i + 1; j < n; j++) {
-					if ((((p.fi & (1ll << i)) == 0) && ((p.fi & (1ll << j))) == 0)) {
-						//건너가지 않은 애들중 2명 고르기
-						ll_ll new_p = { (p.fi | (1ll << i) | (1ll << j)) ,1 };
-						ll new_cost = cost + arr[j];
-						if (dp[new_p.fi][new_p.se] > new_cost) {
-							dp[new_p.fi][new_p.se] = new_cost;
-							brr.push({ new_p,new_cost });
-						}
-					}
-				}
-			}
-
-			for (int i = 0; i < n; i++) {
-				if ((p.fi & (1ll << i)) == 0) {
-					//건너가지 않은 애들중 1명 고르기
-					ll_ll new_p = { (p.fi | (1ll << i)) ,1 };
-					ll new_cost = cost + arr[i];
-					if (dp[new_p.fi][new_p.se] > new_cost) {
-						dp[new_p.fi][new_p.se] = new_cost;
-						brr.push({ new_p,new_cost });
-					}
-				}
+	int ans = 0;
+	for (int i = n - 1; i >= 2; i -= 2) {
+		if (i - 1 >= 2) {
+			int h1 = arr[1] + arr[0] + arr[i] + arr[1];
+			int h2 = arr[i - 1] + arr[0] + arr[i] + arr[0];
+			ans += min(h1,h2);
+			if (i - 1 == 2) {
+				ans += arr[1];
+				break;
 			}
 		}
 		else {
-			for (int i = 0; i < n; i++) {
-				for (int j = i + 1; j < n; j++) {
-					if ((((p.fi & (1ll << i)) != 0) && ((p.fi & (1ll << j))) != 0)) {
-						//건너간 애들중 2명 고르기 (근데 2명 돌려보내면 별로일거 같지만 일단 ㄱㄱ)
-						ll_ll new_p = { (p.fi ^ (1ll << i) ^ (1ll << j)) ,0 };
-						ll new_cost = cost + arr[j];
-						if (dp[new_p.fi][new_p.se] > new_cost) {
-							dp[new_p.fi][new_p.se] = new_cost;
-							brr.push({ new_p,new_cost });
-						}
-					}
-				}
-			}
-
-			for (int i = 0; i < n; i++) {
-				if ((p.fi & (1ll << i)) != 0) {
-					//건너간 애들중 1명 고르기
-					ll_ll new_p = { (p.fi ^ (1ll << i)) ,0 };
-					ll new_cost = cost + arr[i];
-					if (dp[new_p.fi][new_p.se] > new_cost) {
-						dp[new_p.fi][new_p.se] = new_cost;
-						brr.push({ new_p,new_cost });
-					}
-				}
-			}
+			ans += arr[i] + arr[0] + arr[1];
+			break;
 		}
 	}
-
-	cout << dp[(1 << n) - 1][1];
+	cout << ans;
 }
