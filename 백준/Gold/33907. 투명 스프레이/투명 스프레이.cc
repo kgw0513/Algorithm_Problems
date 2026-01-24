8d6c7ee3172efc03304ef1fc7a442bbff7a37fb6
kgw0513 <144788804+kgw0513@users.noreply.github.com>
2026-01-25 00:23:27 +0900

    [Gold I] Title: 투명 스프레이, Time: 312 ms, Memory: 8048 KB -BaekjoonHub

---
+++ 백준/Gold/33907. 투명 스프레이/투명 스프레이.cc
@@ -0,0 +1,135 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <cmath>
+#include <set>
+#include <map>
+#include <bitset>
+#include <utility>
+#include <assert.h>
+#include <unordered_set>
+#include <unordered_map>
+#include <complex>
+#pragma warning(disable:4996)
+#define fi first
+#define se second
+using namespace std;
+
+//rope c++
+//#include <ext/rope>
+//using namespace __gnu_cxx;
+
+/*
+pbds c++ set전용
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+multiset을 쓰고 싶으면 위의 코드에서 마지막 줄만 아래 코드로 변경.(대신 erase가 정상작동 되지 않는다.)
+#define ordered_set tree<int, null_type, less_equal<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+이외에 범위를 늘리고 싶으면 타입변경하면 된다.
+ex. int->ll, less_equal<ll>
+*/
+
+constexpr double PI = 3.1415926535;
+constexpr long long INF = 2147483647;
+constexpr long long lINF = 9000000000000000000;
+constexpr long long nlINF = lINF / 10000;
+constexpr int nINF = 1007483647;
+constexpr long long MOD1 = 1000000007;
+constexpr long long MOD2 = 1000000009;
+constexpr long long MOD3 = 998244353;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<short, short>sh_sh;
+typedef pair<int, int> intint;
+typedef pair<int, double> int_d;
+typedef pair<double, double> dd;
+typedef pair<intint, double> int2_d;
+typedef pair<int, ll> intlong;
+typedef pair<ll, ll> ll_ll;
+typedef pair<intint, int> int2_int;
+typedef pair<int, intint> int_int2;
+typedef pair<ll, ll_ll> ll_ll2;
+typedef pair<ll_ll, ll>ll2_ll;
+typedef pair<intint, intint> int2_int2;
+typedef pair<ll_ll, ll_ll> ll2_ll2;
+typedef pair<char, int> char_int;
+typedef pair<int, char> int_char;
+typedef complex<double> cpx;
+
+int state[702][702];
+int visit[702][702];
+
+intint dxdy[4] = { {-1,0},{1,0},{0,-1},{0,1} };
+bool bfs(int n, int m, int k, int val) {
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			visit[i][j] = 0;
+		}
+	}
+
+	deque<intint>arr;
+	arr.push_back({ 1,1 });
+
+	for (int loop = 0; !arr.empty() && loop <= k; loop++) {
+		int size = arr.size();
+		while (size--) {
+			intint p = arr.front();
+			arr.pop_front();
+			visit[p.fi][p.se] = 2;
+
+			for (intint xy : dxdy) {
+				intint new_p = { p.fi + xy.fi,p.se + xy.se };
+				if (visit[new_p.fi][new_p.se] != 0)continue;
+				if (state[new_p.fi][new_p.se] <= val) {
+					visit[new_p.fi][new_p.se] = 1;
+					size++;
+					arr.push_front(new_p);
+				}
+				else {
+					visit[new_p.fi][new_p.se] = 1;
+					arr.push_back(new_p);
+				}
+			}
+		}
+	}
+	return visit[n][m] == 2;
+}
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	for (int i = 0; i < 702; i++) {
+		for (int j = 0; j < 702; j++) {
+			visit[i][j] = 1;
+		}
+	}
+
+	int n, m, k;
+	cin >> n >> m >> k;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			cin >> state[i][j];
+		}
+	}
+
+	int ans = -1;
+	int s = 0, e = 1000000001;
+	while (s <= e) {
+		int mid = (s + e) / 2;
+		if (bfs(n, m, k, mid)) {
+			ans = mid;
+			e = mid - 1;
+		}
+		else {
+			s = mid + 1;
+		}
+	}
+	cout << ans;
+}
