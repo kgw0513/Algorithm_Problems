a019e1e4eca3c93cc1d99eeeb13001d96da89f7e
kgw0513 <144788804+kgw0513@users.noreply.github.com>
2026-03-13 22:05:13 +0900

    [Gold II] Title: Exam Study Planning, Time: 52 ms, Memory: 64708 KB -BaekjoonHub

---
+++ 백준/Gold/30493. Exam Study Planning/Exam Study Planning.cc
@@ -0,0 +1,158 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <cmath>
+#include <set>
+#include <map>
+#include <bitset>
+#include <utility>
+#include <assert.h>
+#include <unordered_set>
+#include <unordered_map>
+#include <complex>
+#pragma warning(disable:4996)
+#define fi first
+#define se second
+using namespace std;
+
+//rope c++
+//#include <ext/rope>
+//using namespace __gnu_cxx;
+
+/*
+pbds c++ set전용
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+multiset을 쓰고 싶으면 위의 코드에서 마지막 줄만 아래 코드로 변경.(대신 erase가 정상작동 되지 않는다.)
+#define ordered_set tree<int, null_type, less_equal<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+이외에 범위를 늘리고 싶으면 타입변경하면 된다.
+ex. int->ll, less_equal<ll>
+*/
+
+//unordered_set, unordered_map 전용 (구조체로 쓸 경우 == operator도 필요)
+//사용 예시 : unordered_set<intint,pair_hash> arr;
+namespace std {
+    struct default_hash {
+        template <class T1>
+        size_t operator()(const T1 n) const {
+            hash<T1> a{}; size_t sa = a(n);
+            return sa;
+        }
+    };
+    struct pair_hash {
+        template <class T1, class T2>
+        size_t operator()(const pair<T1, T2> n) const {
+            hash<T1> a{}; size_t sa = a(n.fi);
+            hash<T2> b{}; size_t sb = b(n.se);
+            return sa ^ sb;
+        }
+    };
+}
+
+constexpr double PI = 3.1415926535;
+constexpr long long INF = 2147483647;
+constexpr long long lINF = 9000000000000000000;
+constexpr long long nlINF = lINF / 10000;
+constexpr int nINF = 1007483647;
+constexpr long long MOD1 = 1000000007;
+constexpr long long MOD2 = 1000000009;
+constexpr long long MOD3 = 998244353;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<short, short>sh_sh;
+typedef pair<int, int> intint;
+typedef pair<int, double> int_d;
+typedef pair<double, double> dd;
+typedef pair<intint, double> int2_d;
+typedef pair<int, ll> intlong;
+typedef pair<ll, ll> ll_ll;
+typedef pair<intint, int> int2_int;
+typedef pair<int, intint> int_int2;
+typedef pair<ll, ll_ll> ll_ll2;
+typedef pair<ll_ll, ll>ll2_ll;
+typedef pair<intint, intint> int2_int2;
+typedef pair<ll_ll, ll_ll> ll2_ll2;
+typedef pair<char, int> char_int;
+typedef pair<int, char> int_char;
+typedef complex<double> cpx;
+
+ll n;
+ll2_ll2 inputs[2002];
+
+//[0:실패,1:성공][위치][성공 갯수] = 사용할 수 있는 최대 시간
+ll dp[2002][2][2002];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    for (int i = 0; i < 2002; i++) {
+        for (int j = 0; j < 2; j++) {
+            for (int k = 0; k < 2002; k++) {
+                dp[i][j][k] = -nlINF;
+            }
+        }
+    }
+
+    dp[0][0][0] = 0;
+
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> inputs[i].fi.fi >> inputs[i].fi.se >> inputs[i].se.fi >> inputs[i].se.se;
+    }
+
+    for (int i = 0; i < n; i++) { //마지막 위치
+        for (int k = 0; k < n; k++) { // 마지막의 성공 갯수
+            //cout << "dp[" << i << "][" << 0 << "][" << k << "] = " << dp[i][0][k] << "상태.\n";
+            //cout << "dp[" << i << "][" << 1 << "][" << k << "] = " << dp[i][1][k] << "상태.\n";
+
+            int ni = i + 1; //다음 위치
+
+            /*
+            TODO : 고려할 거
+            1. 마지막 실패 - 현재 실패
+            2. 마지막 실패 - 현재 성공
+            3. 마지막 성공 - 현재 실패
+            4. 마지막 성공 - 현재 성공
+            */
+
+            //마지막 실패에 접근이 가능한가
+            if (dp[i][0][k] >= 0) {
+                //마지막 실패 - 현재 실패
+                dp[ni][0][k] = max(dp[ni][0][k], dp[i][0][k] + (inputs[ni].fi.fi - inputs[i].se.fi));
+
+                //마지막 실패 - 현재 성공
+                if (dp[i][0][k] + (inputs[ni].fi.fi - inputs[i].se.fi) >= inputs[ni].se.se) {
+                    dp[ni][1][k + 1] = max(dp[ni][1][k + 1], dp[i][0][k] + (inputs[ni].fi.fi - inputs[i].se.fi) - inputs[ni].se.se);
+                }
+            }
+
+            //마지막 성공에 접근이 가능한가
+            if (dp[i][1][k] >= 0) {
+                //마지막 성공 - 현재 실패
+                dp[ni][0][k] = max(dp[ni][0][k], dp[i][1][k] + (inputs[ni].fi.fi - inputs[i].fi.se));
+
+                //마지막 성공 - 현재 성공
+                if (dp[i][1][k] + (inputs[ni].fi.fi - inputs[i].fi.se) >= inputs[ni].se.se) {
+                    dp[ni][1][k + 1] = max(dp[ni][1][k + 1], dp[i][1][k] + (inputs[ni].fi.fi - inputs[i].fi.se) - inputs[ni].se.se);
+                }
+            }
+        }
+    }
+
+    int ans = 0;
+    for (int i = 1; i <= n; i++) {
+        for (int k = 1; k <= n; k++) {
+            if (dp[i][1][k] >= 0)ans = max(ans, k);
+        }
+    }
+
+    cout << ans;
+}
