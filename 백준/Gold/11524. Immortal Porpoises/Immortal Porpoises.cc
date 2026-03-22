d2a3e58fc4e9db1ba628f0b4ef6dd9cd0013056d
kgw0513 <144788804+kgw0513@users.noreply.github.com>
2026-03-23 07:39:52 +0900

    [Gold II] Title: Immortal Porpoises, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/11524. Immortal Porpoises/Immortal Porpoises.cc
@@ -0,0 +1,136 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <cmath>
+#include <set>
+#include <map>
+#include <bitset>
+#include <utility>
+#include <assert.h>
+#include <unordered_set>
+#include <unordered_map>
+#include <complex>
+#pragma warning(disable:4996)
+#define fi first
+#define se second
+using namespace std;
+
+//rope c++
+//#include <ext/rope>
+//using namespace __gnu_cxx;
+
+/*
+pbds c++ set전용
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+multiset을 쓰고 싶으면 위의 코드에서 마지막 줄만 아래 코드로 변경.(대신 erase가 정상작동 되지 않는다.)
+#define ordered_set tree<int, null_type, less_equal<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+이외에 범위를 늘리고 싶으면 타입변경하면 된다.
+ex. int->ll, less_equal<ll>
+*/
+
+//unordered_set, unordered_map 전용 (구조체로 쓸 경우 == operator도 필요)
+//사용 예시 : unordered_set<intint,pair_hash> arr;
+namespace std {
+    struct default_hash {
+        template <class T1>
+        size_t operator()(const T1 n) const {
+            hash<T1> a{}; size_t sa = a(n);
+            return sa;
+        }
+    };
+    struct pair_hash {
+        template <class T1, class T2>
+        size_t operator()(const pair<T1, T2> n) const {
+            hash<T1> a{}; size_t sa = a(n.fi);
+            hash<T2> b{}; size_t sb = b(n.se);
+            return sa ^ sb;
+        }
+    };
+}
+
+constexpr double PI = 3.1415926535;
+constexpr long long INF = 2147483647;
+constexpr long long lINF = 9000000000000000000;
+constexpr long long nlINF = lINF / 10000;
+constexpr int nINF = 1007483647;
+constexpr long long MOD1 = 1000000007;
+constexpr long long MOD2 = 1000000009;
+constexpr long long MOD3 = 998244353;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<short, short>sh_sh;
+typedef pair<int, int> intint;
+typedef pair<int, double> int_d;
+typedef pair<double, double> dd;
+typedef pair<intint, double> int2_d;
+typedef pair<int, ll> intlong;
+typedef pair<ll, ll> ll_ll;
+typedef pair<intint, int> int2_int;
+typedef pair<int, intint> int_int2;
+typedef pair<ll, ll_ll> ll_ll2;
+typedef pair<ll_ll, ll>ll2_ll;
+typedef pair<intint, intint> int2_int2;
+typedef pair<ll_ll, ll_ll> ll2_ll2;
+typedef pair<char, int> char_int;
+typedef pair<int, char> int_char;
+typedef complex<double> cpx;
+
+constexpr ll MOD = 1000000000;
+
+vector<vector<ll>>& operator*=(vector<vector<ll>>& a,const vector<vector<ll>>& b) {
+    vector<vector<ll>> c(2, vector<ll>(2, 0));
+    assert(a.size() == 2 && a[0].size() == 2 && a[1].size() == 2);
+    assert(b.size() == 2 && b[0].size() == 2 && b[1].size() == 2);
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            for (int k = 0; k < 2; k++) {
+                c[i][j] += a[i][k] * b[k][j];
+                c[i][j] %= MOD;
+            }
+        }
+    }
+
+    a = move(c);
+
+    return a;
+}
+
+vector<vector<ll>> arrpown(const vector<vector<ll>>arr, ll n) {
+    if (n == 1)return arr;
+    vector<vector<ll>> h = move(arrpown(arr, n / 2));
+    h *= h;
+    //cout << n << ":" << h[0][0] << "," << h[0][1] << "," << h[1][0] << "," << h[1][1] << ".1\n";
+    if (n % 2 == 1) {
+        h *= arr;
+        //cout << n << ":" << h[0][0] << "," << h[0][1] << "," << h[1][0] << "," << h[1][1] << ".2\n";
+    }
+    return h;
+}
+
+ll cal(ll x) {
+    if (x <= 2)return 1;
+    ll n = x - 2;
+    vector<vector<ll>> h = arrpown({ {1,1},{1,0} }, n);
+    return (h[0][0] + h[0][1]) % MOD;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t, a, b;
+    cin >> t;
+    while (t--) {
+        cin >> a >> b;
+        cout << a << " " << cal(b) << "\n";
+    }
+}
